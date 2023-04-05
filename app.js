cb8a07ebbe891bcbf5d408340b63d134f283ad91
Ayesha Akram <128508163+Ayesha-Akram@users.noreply.github.com>
2023-04-05 12:20:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+//Q. no 01)
+
+let username;
+
+// Q. no 02)
+
+let myName = ("AyeshaAkram");
+
+// Q. no 03)
+
+let titledMessage;
+let titleMessage = ("Hello World");
+alert("tittleMessage");
+
+//Q. no 04);
+
+let fullName = "AyeshaAkram";
+let Qualification = "UnderGraduate";
+let Age = "21";
+let rollNumber = "101799";
+
+alert("fullName");
+alert("Qualification");
+alert("Age");
+alert("RollNumber");
+
+//Q. no 05);
+
+let Pizza = "PIZZA/nPIZZ/nPIZ/nPI/nP";
+alert("Pizza");
+
+//Q. no 06);
+
+let email = "Ayeshaakram437@gmail.com";
+alert("my email address is " + "Ayeshaakram437@gmail.com");
+
+//Q. no 07); 
+
+let book = "A smarter way to learn javaScript";
+alert("I am trying to learn from the Book A smarter\n way to learn JavaScript");
+
+//Q. no 09);
+
+var pattern;
+pattern = "▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ஜ۩۞۩ஜ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬";
+alert (pattern);
